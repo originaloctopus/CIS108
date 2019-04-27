eea70fc83993b3fdde30b6566037ee6904606b50
lgclcrtv <46700795+originaloctopus@users.noreply.github.com>
2019-04-27 11:56:04 -0700

    initial commit

---
+++ CIS108_Lab10/Exercise1/Main.cpp
@@ -0,0 +1,29 @@
+// Jeremy Page
+// Lab 10: RPG Classes
+
+#include <iostream>
+#include <random>
+using namespace std;
+
+string name[4] = {"Igor", "Johnathan", "Magdelena", "Thor", };
+
+class character {
+	string name, gender;
+	int health, magika;
+
+public:
+	void player_create(string, string, int, int);
+};
+
+void character::player_create(string name, string gender, int x, int y) {
+	string user_name;
+	string user_gender;
+	int health;
+	int magika;
+}
+
+int main() {
+	character first_character;
+	
+
+}
